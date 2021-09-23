23b27f87846aaa1419d8f8674b2097d0915b84b2
BenW00 <ispyb2000@gmail.com>
2021-09-23 01:53:45 -0400

    Finished exercise except the bonus

---
+++ script.js
@@ -0,0 +1,28 @@
+// Question 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+
+// Question 1b
+console.log(warmHugs.toUpperCase())
+
+// Quesiton 1c
+warmHugs = warmHugs.replace("like", "love")
+
+// Question 2a
+let beenImpaled = "Oh, look at that. I've been impaled."
+
+// Question 2b
+console.log(beenImpaled.substring(18,36))
+
+// Question 3a
+let dotDotDot = "..."
+
+// Question 3b
+let skullBones = `I don't have a skull${dotDotDot}or bones`
+
+// Quesiton 4
+console.log(Math.PI);
+
+// Question 5
+let randomNumber = Math.floor(Math.random() * 4)
+
+// Bonus
